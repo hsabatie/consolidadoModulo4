fb36613d2c994d1cedc89ada98ab8dfc5e13888c
Hector <sabatie@live.cl>
2024-10-13 00:58:29 -0300

    metodos de carrito

--- assest/script.js
+++ assest/script.js
@@ -20,6 +20,29 @@ class Productos {
 class Carrito{
     constructor(){
         this.producto = [];
+    } //metodo para agregar productos a el carrito
+    agregarProducto(producto, cantidad) {
+        const existe = this.producto.find(item => item.nombre === producto.nombre);//find busca en el arreglo
+        if (existe) {
+            existe.cantidad +=cantidad;  //si ya existe se suma a la cantidad          
+        } else {
+            this.producto.push({ nombre: producto.nombre, precio: producto.precio, cantidad }); //si no, se agrega como nuevo
+        }
+    }
+    calculatrTotal(){
+        let total=0;
+        this.producto.forEach(item=>{
+            total += item.precio * item.cantidad;
+        })
+        return total;
+    }
+    mostrarBoleta(){
+        let boleta =  "Compras lo siguiente:\n";
+        this.producto.forEach(item=>{
+            boleta +=`${item.nombre}- Cantidad ${item.cantidad} -Punit ${item.precio}- Vtotal ${item.cantidad * item.precio}\n`
+        })
+        boleta += `Total a pagar $${this.calculatrTotal()} \n Gracias por su compra`
+        return boleta;
     }
 }
 // creo los objetos de la lista productos llamando a la clase Productos y dejando estos en la variable listaProductos.
@@ -30,5 +53,11 @@ let listaProductos = [
     new Productos("Mermelada", 890),
     new Productos("Azúcar", 1300)
 ];
-//console.log(listaProductos);
+//creo objeto del carrito
+const carrito = new Carrito();
 
+carrito.agregarProducto(listaProductos[0], 2); // Agregar 2 unidades de "Leche"
+carrito.agregarProducto(listaProductos[1], 3); // Agregar 3 unidades de "Pan de Molde"
+
+console.log(carrito.calculatrTotal());
+alert(carrito.mostrarBoleta())
